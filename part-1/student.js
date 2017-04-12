b5686ad44ccc7f900199c0969c2a0b4b5e513958
Julia(e) Xu <email": null>
2017-04-12 09:30:27 -0700

    Part 1: Create averageScore function that rounds down to nearest whole number

--- part-1/student.js
+++ part-1/student.js
@@ -2,3 +2,12 @@ var Student = function(studentInfo) {
   this.firstName = studentInfo.firstName;
   this.scores = studentInfo.scores;
 }
+
+Student.prototype.averageScore = function() {
+  var sum = 0;
+  for(var i = 0; i < this.scores.length; i++ ) {
+    sum += this.scores[i];
+  }
+  var average = (sum / this.scores.length);
+  return Math.floor(average);
+}

71792699b01790d3b823054568c3f57dee9b0c13
Linsey R <email": null>
2017-04-12 15:28:12 -0700

    redo login to redirect to an actual login page

--- part-3/app/controllers/sessions_controller.rb
+++ part-3/app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
+get '/login' do
+  erb :'partials/_login'
+end
+
 post '/login' do
   @user = User.find_by(email: params[:email])
   if @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect '/'
+  else
+    redirect to '/login'
   end
 end
 
-get '/login' do
-  redirect '/'
-end
 
 post '/logout' do
   session[:user_id] = nil

fe64a0e0bdfb36facc4f4262751800485d44a015
Julia(e) Xu <email": null>
2017-04-12 11:48:20 -0700

    Part 3: Ensure username is also unique

--- part-3/app/models/user.rb
+++ part-3/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 
   include BCrypt
 
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_hash, presence: true
 

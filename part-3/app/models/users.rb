31c9e14afd54ef2e657c419089b7da18d8aa584c
Nick Jarin <email": null>
2017-04-12 11:52:05 -0700

    Add validations for User

--- part-3/app/models/users.rb
+++ part-3/app/models/users.rb
@@ -1,3 +1,7 @@
 class Users < ActiveRecord::Base
-  # Remember to create a migration!
+  validates_presence_of :email, { message: "An email is required." }
+  validates_presence_of :username, { message: "A username is required." }
+  validates_presence_of :password, { message: "A password is required." }
+  validates :email, uniqueness: { case_sensitive: false }, message: "That email is already registered"
+  validates :username, uniqueness: { case_sensitive: false }, message: "That username is already registered"
 end

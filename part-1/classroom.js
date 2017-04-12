34f78801aca07845ce6baa7e5ccbabd16c0f6c5c
Eitan Greene <email": null>
2017-04-12 10:09:59 -0700

    Find honor roll students

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -1,8 +1,15 @@
 var Classroom = function(students) {
   this.students = students;
-  this.find = function(firstName) {
-    return students.find(function(student) {
-      return student.firstName === firstName;
-    })
-  }
+}
+
+Classroom.prototype.find = function(firstName) {
+  return this.students.find(function(student) {
+    return student.firstName === firstName;
+  })
+}
+
+Classroom.prototype.honorRollStudents = function() {
+  return this.students.filter(function(student) {
+    return student.averageScore() >= 95;
+  })
 }

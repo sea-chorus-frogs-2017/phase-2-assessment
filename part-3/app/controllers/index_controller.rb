1bff015541494605313b19b450ef6223683a643e
Dan McKeon <email": null>
2017-04-12 14:34:41 -0700

    addlogout functionality

--- part-3/app/controllers/index_controller.rb
+++ part-3/app/controllers/index_controller.rb
@@ -1,3 +1,4 @@
 get '/' do
+  @user = User.find(session[:user_id]) if session[:user_id]
   erb :index
 end
--- part-3/app/controllers/sessions_controller.rb
+++ part-3/app/controllers/sessions_controller.rb
@@ -1,14 +1,24 @@
-get 'sessions/new' do
-  erb 'sessions/new'
+get '/sessions/new' do
+  erb :'sessions/new'
 end
 
 post '/sessions' do
-  @user = User.find(params[:email])
-
+  @user = User.find_by(email: params[:email])
   if @user
-    if @user.authenticate?(password_input)
-
+    if @user.authenticate?(params[:password])
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      @error = "Password is incorrect"
+      erb :'sessions/new'
+    end
   else
-
+    @error = "Username does not exist"
+    erb :'sessions/new'
   end
+end
+
+delete '/sessions/:id' do
+  session.destroy
+  redirect '/'
 end

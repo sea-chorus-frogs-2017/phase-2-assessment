d102df072cfaeb0b01a6d20efb54c1f086af3552
Austin Kading <email": null>
2017-04-12 15:52:09 -0700

    adds user model

--- part-3/app/models/user.rb
+++ part-3/app/models/user.rb
@@ -1,3 +1,28 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+   include BCrypt
+
+ validates :username, :email, :password_hash, presence: true
+ validates :username, :email, uniqueness: true
+
+
+ def password
+   @password ||= Password.new(password_hash)
+ end
+
+ def password=(new_password)
+   @password = Password.create(new_password)
+   self.password_hash = @password
+ end
+
+ def self.authenticate(username, password)
+   user = User.find_by(username: username)
+   return nil unless user
+   if user.password == password
+     user
+   else
+     nil
+   end
+ end
 end

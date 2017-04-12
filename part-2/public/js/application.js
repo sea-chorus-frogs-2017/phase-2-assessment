7a6cab83cad418d2c924675cd8a234134d93b5db
Eitan Greene <email": null>
2017-04-12 11:05:16 -0700

    Dynamically updates posts's like count

--- part-2/public/js/application.js
+++ part-2/public/js/application.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
       data: $(this).serialize()
     }).done(function(response) {
       $("#post-list").prepend(response);
+    });
+  });
+
+  $('#post-list').on('submit', '.new-post-like-form', function(event) {
+    event.preventDefault();
+    var that = this;
+    $.ajax({
+      url: $(this).attr('action'),
+      type: "PUT"
+    }).done(function(response) {
+      var likeCount = $(that).prev().children()[1];
+      $(likeCount).html(response + " likes")
     })
   })
-})
+});

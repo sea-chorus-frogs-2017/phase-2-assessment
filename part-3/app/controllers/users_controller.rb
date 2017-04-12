057d2cbca2b520423bac44fa479e9af228ca4fb2
Nick Jarin <email": null>
2017-04-12 12:23:23 -0700

    implement errors for registration

--- part-3/app/controllers/users_controller.rb
+++ part-3/app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ get '/users/new' do
 end
 
 post '/users' do
-  # @user = User.new()
+  @user = User.new(params[:user])
+
+  if @user.save
+    redirect "/"
+  else
+    erb :"users/new"
+  end
 end

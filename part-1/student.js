b6c7c631b41fe37046b80f1aa96dd341a720e485
Jordan Levine <email": null>
2017-04-12 09:52:11 -0700

    Add averageScore method

--- part-1/student.js
+++ part-1/student.js
@@ -2,3 +2,12 @@ var Student = function(student) {
   this.firstName = student.firstName;
   this.scores = student.scores;
   }
+
+
+
+
+  Student.prototype.averageScore = function() {
+    var scoreSum = this.scores.reduce((previous, current) => current += previous);
+    var avg = scoreSum / this.scores.length;
+    return Math.floor(avg);
+  }

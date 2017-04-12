6f6b9738ff275603a024ba0908d7f83881fba4fd
Julia(e) Xu <email": null>
2017-04-12 11:22:00 -0700

    Part 3: Create user registration

---
+++ part-3/app/models/user.rb
@@ -0,0 +1,25 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+
+  include BCrypt
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, entered_password)
+    user = User.find_by(email: email)
+    return user if user && user.password == entered_password
+    nil
+  end
+
+end

b3caf8e3e419da94dd1cd3acd8d54864fdba3520
Dan McKeon <email": null>
2017-04-12 11:33:13 -0700

    add bcrypt methods to user model

--- part-3/app/models/user.rb
+++ part-3/app/models/user.rb
@@ -1,4 +1,17 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end

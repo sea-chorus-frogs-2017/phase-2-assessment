f95251390f764a112415e983eaa39d49286be878
Matthew Fischer <email": null>
2017-04-12 16:42:51 -0700

    Show only current auctions

--- part-3/app/controllers/index.rb
+++ part-3/app/controllers/index.rb
@@ -1,3 +1,9 @@
 get '/' do
+  todays_time = Time.now.utc.iso8601
+  all_auctions = Auction.all.order(:starts_at)
+
+  @current_auctions = all_auctions.select do |auction|
+    (auction.starts_at.to_time.to_s < todays_time) && (auction.ends_at.to_time.to_s > todays_time)
+  end
   erb :'index'
 end

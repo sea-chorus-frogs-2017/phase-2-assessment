70b2275650110c0e24dee4f8aa9eb8fc70cbeae1
Jordan Levine <email": null>
2017-04-12 11:31:20 -0700

    Confirm app runs and javascript code is being hit

---
+++ part-2/public/js/application.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  console.log('in javascript code!');
+
+  // $('#new-post-form').submit(function(event) {
+  //   // user is submitting filled-out post form
+  //   event.preventDefault();
+  //   console.log(' user submitted a new post form - listener is working');
+    // $.ajax({
+    //   url: '/posts',
+    //   method: 'POST',
+    //   data:$(event.target).serialize()
+    // }).done(function(response) {
+    //   $('#horse-container').append(response);
+    //   $('#new-horse-form').empty();
+    //   $('#add-horse').removeClass('invisible');
+    //   });
+  // });
+
+// $('#add-horse').submit(function(event) {
+//   event.preventDefault();
+//   $.ajax({
+//       url: '/horses/new',
+//       method: 'GET'
+//     }).done(function(response) {
+//       $('#add-horse').addClass('invisible');
+//       $('#new-horse-form').html(response);
+//       });
+// });
+
+// $('#new-horse-form').submit(function(event) {
+//     // user is submitting filled-out horse form
+//     event.preventDefault();
+//     $.ajax({
+//       url: '/horses',
+//       method: 'POST',
+//       data:$(event.target).serialize()
+//     }).done(function(response) {
+//       $('#horse-container').append(response);
+//       $('#new-horse-form').empty();
+//       $('#add-horse').removeClass('invisible');
+//       });
+//   });
+
+//   $("#horse-container a").click(function(event) {
+//     event.preventDefault();
+//     var horseLink = $(event.currentTarget);
+//     var href = horseLink.attr('href');
+//     var horseParentLi = horseLink.parent()
+//      // note: data-horse-id field has been added to li element
+//     // in _horse_element.erb; this tag could be used instead to
+//     // determine horseId
+//     // var horseId = horseParentLi.attr('data-horse-id')
+//     var horseId = href[href.length -1];
+//      $.ajax({
+//       url: '/horses/' + horseId,
+//       method: 'GET',
+//     }).done(function(response) {
+//       horseParentLi.append(response);
+//     });
+//   });
+});
+
+

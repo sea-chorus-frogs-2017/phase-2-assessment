ec216ff4354823103896ae320a35fd4ee1695743
Eitan Greene <email": null>
2017-04-12 16:25:00 -0700

    Updates item

--- part-3/app/controllers/items.rb
+++ part-3/app/controllers/items.rb
@@ -11,3 +11,19 @@ post "/items" do
     erb :"/items/new"
   end
 end
+
+put '/items/:id' do
+  @item = Item.find(params[:id])
+  @item.assign_attributes(params[:item])
+  if @item.save
+    redirect "/users/#{session_user.id}"
+  else
+    erb :"/items/update"
+  end
+
+end
+
+get '/items/:id/edit' do
+  @item = Item.find_by(id: params[:id])
+  erb :'/items/update'
+end
---
+++ part-3/app/helpers/item_time_helpers.rb
@@ -0,0 +1,5 @@
+helpers do
+  def date_and_time(datetime)
+    datetime.strftime("%Y-%m-%dT%I:%M")
+  end
+end

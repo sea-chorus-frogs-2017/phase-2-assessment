9dedd8499fc658bca191645e75baa10c37451edc
Austin Kading <email": null>
2017-04-12 10:49:20 -0700

    something wierd is going on with find method

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -1,3 +1,14 @@
 var Classroom = function(students) {
   this.students = students;
 }
+
+Classroom.prototype.find = function(name) {
+  var student = [];
+
+   for(var i = 0; i < this.students.length; i++){
+      if(this.students[i].firstName === name){
+        return student.push(this.students[i]);
+      }
+      return student;
+    }
+}

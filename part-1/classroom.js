5f2630bd395d919718c3d0863dff9882de973832
Jordan Levine <email": null>
2017-04-12 10:38:51 -0700

    Write honorRollStudents method

--- part-1/classroom.js
+++ part-1/classroom.js
@@ -13,48 +13,10 @@ Classroom.prototype.find = function(firstNameToFind) {
   return this.students.filter(studentsThisName)[0];
 }
 
-// garden.flowersByName = function(nameToFind) {
-//   var flowersThisName = function(el) {
-//     return (el.name === nameToFind);
-//   }
-//   return this.flowers.filter(flowersThisName);
-// }
-
-// var garden = {}
-
-// garden.name = 'Kula Botanical Garden';
-// garden.location = 'Makawao';
-// garden.flowers = [];
-
-// garden.plant = function(flowers) {
-//   var i;
-//   for(i=0; i < flowers.length; i++) {
-//     var newFlower = flowers[i];
-//     this.addFlower(newFlower);
-//   }
-// }
-
-// garden.addFlower = function(flower) {
-//     this.flowers.push(flower);
-// }
-
-// garden.remove = function(flower) {
-//   var flowerIndex = this.flowers.indexOf(flower);
-//   this.flowers.splice(flowerIndex,1);
-// }
-
-// garden.flowersByColor = function(colorToFind) {
-//   var flowersThisColor = function(el) {
-//     return (el.color === colorToFind);
-//   }
-//   return this.flowers.filter(flowersThisColor);
-// }
-
-// garden.flowersByName = function(nameToFind) {
-//   var flowersThisName = function(el) {
-//     return (el.name === nameToFind);
-//   }
-//   return this.flowers.filter(flowersThisName);
-// }
-
+Classroom.prototype.honorRollStudents = function() {
+  var honorRoll = function(el) {
+    return (el.averageScore() >= 95);
+  }
+  return this.students.filter(honorRoll);
+}
 

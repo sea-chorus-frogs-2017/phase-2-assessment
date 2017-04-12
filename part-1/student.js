77a0a4b7646647234fd46c2821b12af1a486f822
Jordan Levine <email": null>
2017-04-12 10:09:00 -0700

    Add letterGrade method

--- part-1/student.js
+++ part-1/student.js
@@ -11,3 +11,24 @@ var Student = function(student) {
     var avg = scoreSum / this.scores.length;
     return Math.floor(avg);
   }
+
+Student.prototype.letterGrade = function() {
+  avgScore = this.averageScore();
+  switch(true) {
+    case (avgScore >= 90):
+      return 'A';
+      break;
+    case (avgScore >= 80):
+      return 'B';
+      break;
+    case (avgScore >= 70):
+      return 'C';
+      break;
+    case (avgScore >= 60):
+      return 'D';
+      break;
+    default:
+      return 'E';
+      break;
+  }
+}

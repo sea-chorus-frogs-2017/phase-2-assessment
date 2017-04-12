d1bc1b821b70f2eb5ec6549eb31e92c2a64bc240
Eitan Greene <email": null>
2017-04-12 15:35:29 -0700

    Creates new items

--- part-3/app/controllers/items.rb
+++ part-3/app/controllers/items.rb
@@ -1,3 +1,13 @@
 get "/items/new" do
   erb :"/items/new"
 end
+
+post "/items" do
+  p params
+  @item = Item.new(params[:item])
+  if @item.save
+    redirect "/users/#{session_user.id}"
+  else
+    erb :"/items/new"
+  end
+end

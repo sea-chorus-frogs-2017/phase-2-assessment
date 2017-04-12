b1c6a2e3e8c01eaf3827bc59e823a9ed1573671a
Jordan Levine <email": null>
2017-04-12 10:24:54 -0700

    Create classroom constructor object

---
+++ part-1/classroom.js
@@ -0,0 +1,46 @@
+var Classroom = function(incomingStudents) {
+  this.students = [];
+  var i;
+  for(i=0; i < incomingStudents.length; i++) {
+      this.students.push(incomingStudents[i]);
+  }
+}
+
+// var garden = {}
+
+// garden.name = 'Kula Botanical Garden';
+// garden.location = 'Makawao';
+// garden.flowers = [];
+
+// garden.plant = function(flowers) {
+//   var i;
+//   for(i=0; i < flowers.length; i++) {
+//     var newFlower = flowers[i];
+//     this.addFlower(newFlower);
+//   }
+// }
+
+// garden.addFlower = function(flower) {
+//     this.flowers.push(flower);
+// }
+
+// garden.remove = function(flower) {
+//   var flowerIndex = this.flowers.indexOf(flower);
+//   this.flowers.splice(flowerIndex,1);
+// }
+
+// garden.flowersByColor = function(colorToFind) {
+//   var flowersThisColor = function(el) {
+//     return (el.color === colorToFind);
+//   }
+//   return this.flowers.filter(flowersThisColor);
+// }
+
+// garden.flowersByName = function(nameToFind) {
+//   var flowersThisName = function(el) {
+//     return (el.name === nameToFind);
+//   }
+//   return this.flowers.filter(flowersThisName);
+// }
+
+

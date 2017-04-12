2f7c82e9fd29e3212e9eeb7a354f5f2ba53958eb
Austin Kading <email": null>
2017-04-12 10:08:41 -0700

    adds average score method to student

--- part-1/student.js
+++ part-1/student.js
@@ -2,3 +2,13 @@ var Student = function(student) {
   this.firstName = student.firstName;
   this.scores = student.scores;
 }
+
+Student.prototype.averageScore = function() {
+  var total = 0;
+  for(var i = 0; i < this.scores.length; i++) {
+      total += this.scores[i];
+  }
+  var avg = total / this.scores.length;
+  return Math.floor(avg);
+}
+

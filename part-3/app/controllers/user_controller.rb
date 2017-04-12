922820486754fe0d033055dac6ff433a9dce9de8
Dillon Estelle <email": null>
2017-04-12 14:39:46 -0700

    error for incorrect email or password on login and logout function added

--- part-3/app/controllers/user_controller.rb
+++ part-3/app/controllers/user_controller.rb
@@ -11,16 +11,41 @@ get '/users/login' do
   if request.xhr?
     status 200
   else
+    erb :'/users/_login_form', locals: {errors: false}
+  end
+end
+
+get '/users/logout' do
+  session.delete(:username)
+  redirect to '/'
+end
+
+
+get '/users/login' do
+  @user = User.new
+  if request.xhr?
+    status 200
+  else
     erb :'/users/_login_form'
+  end
+end
+
+post '/users/login' do
+  user = User.authenticate(params['user']['email'], params['user']['password'])
+  if user
+    session[:username] = user.username
+    redirect to ('/')
+  else
+
+    erb :'users/_login_form', locals: {errors: true}
   end
 end
 
 post '/users' do
     @user = User.new(params['user'])
-
     if @user.save
-      erb :'/index'
-
+      session[:username] = @user.username
+      redirect to '/'
     else
       erb :'/users/new'
   end

8b9692bbe46351e4dca3388337be7b4093435e8b
Matthew Fischer <email": null>
2017-04-12 14:56:36 -0700

    Create user profile and 404 page

--- part-3/app/controllers/users_controller.rb
+++ part-3/app/controllers/users_controller.rb
@@ -2,3 +2,13 @@ get '/users/new' do
   @user
   erb :'users/new'
 end
+
+get '/users/:id' do
+  @user = User.find_by_id(params[:id])
+  # I am assuming a user can only view their own page
+  if @user && (session[:user_id] == @user.id)
+    erb :'users/show'
+  else
+    erb :'404'
+  end
+end

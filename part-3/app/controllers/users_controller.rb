8949477c4ada7da5c664dd1e3f37d493b98b1c87
Jordan Levine <email": null>
2017-04-12 16:53:16 -0700

    Save a user if entered correctly

--- part-3/app/controllers/users_controller.rb
+++ part-3/app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
-# enable :sessions
+enable :sessions
 
-# post '/users' do
-#   user = User.new(params[:user])
-#   user.save
-#   if user.persisted?
-#     session[:user_id] = user.id
-#     redirect '/'
-#   else
-#     erb :"_registration-form"
-#   end
-# end
+post '/users' do
+  user = User.new(params[:user])
+  user.save
+  if user.persisted?
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    erb :"/users/_registration"
+  end
+end
 
 get '/users/new' do
   erb :"users/_registration"

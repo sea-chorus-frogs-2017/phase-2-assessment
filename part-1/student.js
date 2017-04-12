f2f5ce3d138d0ebd4c8b0a7bee366247fb266559
Austin Kading <email": null>
2017-04-12 10:23:37 -0700

    adds letter grade method

--- part-1/student.js
+++ part-1/student.js
@@ -12,3 +12,18 @@ Student.prototype.averageScore = function() {
   return Math.floor(avg);
 }
 
+Student.prototype.letterGrade = function() {
+    if(this.averageScore() >= 90) {
+      return "A";
+    }else if(this.averageScore() >= 80 && this.averageScore() < 90) {
+      return "B"
+    }else if(this.averageScore() >= 70 && this.averageScore() < 80) {
+      return "C"
+    }else if(this.averageScore() >= 60 && this.averageScore() < 70) {
+      return "D"
+    }else {
+      return "F"
+    }
+  }
+
+

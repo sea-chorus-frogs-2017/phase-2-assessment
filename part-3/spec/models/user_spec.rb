b760d519c5399ecad6b693426242d0b78e4bc8f3
Nick Jarin <email": null>
2017-04-12 12:12:02 -0700

    Add and pass tests for User validations

--- part-3/spec/models/user_spec.rb
+++ part-3/spec/models/user_spec.rb
@@ -8,10 +8,19 @@ describe User do
     let!(:good_user){User.create!(good_params)}
 
     it 'creates a User given proper params' do
-      expect(User.last).to eq good_user
+      expect(User.first).to eq good_user
     end
 
+    it 'does not create User without an email' do
+      expect{ User.create!(username: "Looper", password: "123") }.to raise_error
+    end
 
-    it ''
+    it 'does not create User without a username' do
+      expect{ User.create!(email: "yo@yo.org", password: "123") }.to raise_error
+    end
+
+    it 'does not create User without a password' do
+      expect{ User.create!(email: "yo@yo.org", username: "Looper") }.to raise_error
+    end
   end
 end

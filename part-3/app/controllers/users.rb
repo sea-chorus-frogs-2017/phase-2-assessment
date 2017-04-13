4256bf8410c2a67f18ea4b227fa60aab0164c730
Philip Cornman <pjc5108@gmail.com>
2017-04-12 17:37:21 -0700

    list items on user page.

--- part-3/app/controllers/users.rb
+++ part-3/app/controllers/users.rb
@@ -12,5 +12,7 @@ end
 
 get '/users/:username' do
   @username = params[:username]
+  @auctions = Auction.all
+  p Auction.all
   erb :'users/show'
 end
---
+++ part-3/db/migrate/20170412171550_add_description_to_auctions.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToAuctions < ActiveRecord::Migration
+  def change
+    add_column :auctions, :description, :string
+  end
+end

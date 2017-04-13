072c75b374d8a008d2a396b67cd1a27aa4d04cc0
Matthew Fischer <email": null>
2017-04-12 17:37:01 -0700

    Add ability to bid

--- part-3/app/models/bid.rb
+++ part-3/app/models/bid.rb
@@ -3,4 +3,11 @@ class Bid < ActiveRecord::Base
   belongs_to :auction_item, class_name: "Auction"
 
   validates :amount, presence: true
+  validate :bid_cant_be_less_than_1
+
+  def bid_cant_be_less_than_1
+    if self.amount < 1.to_s
+      errors.add(:amount, "can't be less than $1")
+    end
+  end
 end

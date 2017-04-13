62f01744650eabe365e9ed0bf8f7e21da4333259
Dan McKeon <email": null>
2017-04-12 17:27:18 -0700

    add bidding feature

--- part-3/app/controllers/items_controller.rb
+++ part-3/app/controllers/items_controller.rb
@@ -14,8 +14,13 @@ get '/items/new' do
 end
 
 get '/items/:id' do
-  @user = User.find(session[:user_id]) if session[:user_id]
   @item = Item.find(params[:id])
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+    @user_bid = Bid.find_by(bidder_id: @user.id)
+  else
+    @user_bid = false;
+  end
   erb :'items/show'
 end
 

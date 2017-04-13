76381488461706fb8e37c279d964e37f1904af78
Linsey R <email": null>
2017-04-12 17:36:48 -0700

    add Index and Show page for an item, as well as spec file

--- part-3/app/controllers/items_controller.rb
+++ part-3/app/controllers/items_controller.rb
@@ -1,3 +1,13 @@
+get '/items' do
+  @items = Item.all
+  erb :'items/index'
+end
+
+get '/items/:id' do
+  @item = Item.find(params[:id])
+  erb :'items/show'
+end
+
 get '/users/:user_id/items/new' do
   @user = User.find(params[:user_id])
   erb :'items/new'
@@ -10,3 +20,10 @@ post '/users/:user_id/items' do
   redirect to "/users/#{@user.id}"
 end
 
+put '/users/:user_id/items/:id' do
+  # This is the route to update the specific item
+end
+
+delete '/users/:user_id/items/:id' do
+  # This is the route to delete the specific item
+end
---
+++ part-3/spec/models/item_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+
+# WRITE TESTS TO TEST THE ITEMS MODEL & ASSOCIATION WITH USER MODEL!
+describe Item do
+end
